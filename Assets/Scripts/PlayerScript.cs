22b305fbf5857dd7fc71e2662072f636bb0b4ebc
Netzia <Netzia@users.noreply.github.com>
2017-11-25 14:59:24 +0200

    asdasd

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -14,64 +14,35 @@ public enum PlayerType
 public class PlayerScript : MonoBehaviour {
 
 	public float initial_speed;
+    public float current_speed;
+    public float acceleration;
     public float speed_factor;
     Rigidbody2D rb;
     public PlayerType PlayerTag;
     private float sliderValue=0.5f;
-    private float rotateFactor = 0;
+    private int rotateFactor = 0;
     
 
 	// Use this for initialization
 	void Start () {
-        
+        current_speed = initial_speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += transform.right * initial_speed * Time.deltaTime;
-        
-        switch (PlayerTag)
-        {
-            case PlayerType.LEFT:
-                if (sliderValue > 0.5)
-                {
-                    print("Rotate Right");
-                    rotateFactor = +1f;
-                }
-                else if (sliderValue < 0.5)
-                {
-                    print("Rotate Left");
-                    rotateFactor = -1f;
-                }
-                else
-                {
-                    print("No Rotation");
-                    rotateFactor = 0;
-                }
-                break;
+        current_speed += acceleration * Time.deltaTime;
+        transform.position += transform.right * current_speed * Time.deltaTime;
 
-            case PlayerType.RIGHT:
-                if (sliderValue > 0.5)
-                {
-                    print("Rotate Left");
-                    rotateFactor = +1f;
-                }
-                else if (sliderValue < 0.5)
-                {
-                    print("Rotate Right");
-                    rotateFactor = -1f;
-                }
-                else
-                {
-                    print("No Rotation");
-                    rotateFactor = 0;
-                }
-                break;
-        }
+        if (sliderValue > 0.5f)
+            rotateFactor = 1;
+        else if (sliderValue < 0.5f)
+            rotateFactor = -1;
+        else
+            rotateFactor = 0;
 
         float diff = Mathf.Abs(sliderValue - 0.5f);
         diff *= 200;
-        transform.Rotate(0, 0, diff * rotateFactor * Time.deltaTime);
+        transform.Rotate(0, 0, diff * (float)rotateFactor * Time.deltaTime);
     }
 
 
