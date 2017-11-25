7dde76cd6845e9cd1135ac50432868a1a7c37d7d
Netzia <Netzia@users.noreply.github.com>
2017-11-26 00:03:30 +0200

    added some art assets

--- Assets/Scripts/MaskShrink.cs
+++ Assets/Scripts/MaskShrink.cs
@@ -6,22 +6,24 @@ using UnityEngine.UI;
 public class MaskShrink : MonoBehaviour {
 
     public RectTransform maskRect;
-    public BoxCollider2D triggerCollision;
+    public PolygonCollider2D triggerCollision;
     public Image maskImage;
+    public GameManager gameManager;
     public float shrinkingPeriod;
     public float minIceFieldScale;
     private float currentTime;
     Vector3 defaultScale;
-
+    Vector3 triggerCollisionDefaultScale;
+    
 
 
     private void OnEnable()
     {
-        GameManager.Instance.onResetEvent += this.resetScales;
+        gameManager.onResetEvent += this.resetScales;
     }
     private void OnDisable()
     {
-        GameManager.Instance.onResetEvent -= this.resetScales;
+        gameManager.onResetEvent -= this.resetScales;
     }
 
 
@@ -30,7 +32,8 @@ public class MaskShrink : MonoBehaviour {
     {
         currentTime = 0.0f;
         defaultScale = maskRect.transform.localScale;
-	}
+        triggerCollisionDefaultScale = triggerCollision.transform.localScale;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -38,13 +41,17 @@ public class MaskShrink : MonoBehaviour {
         currentTime += Time.deltaTime;
         float shrinkFactor = Mathf.Min(currentTime / shrinkingPeriod, 1.0f);
         Vector3 maskScale = (1.0f - shrinkFactor) * defaultScale;
+        Vector3 collisionScale = (1.0f - shrinkFactor) * triggerCollisionDefaultScale;
 
         maskScale.x = Mathf.Max(maskScale.x, minIceFieldScale);
         maskScale.y = Mathf.Max(maskScale.y, minIceFieldScale);
         maskScale.z = Mathf.Max(maskScale.z, minIceFieldScale);
+        collisionScale.x = Mathf.Max(collisionScale.x, 0.0001f);
+        collisionScale.y = Mathf.Max(collisionScale.y, 0.0001f);
+        collisionScale.z = Mathf.Max(collisionScale.z, 0.0001f);
 
         maskRect.transform.localScale = maskScale;
-        triggerCollision.size = new Vector2(maskScale.x * 0.93f, maskScale.y * 0.93f);
+        triggerCollision.transform.localScale = collisionScale;
         maskImage.transform.localScale = new Vector3(1.0f / maskScale.x, 1.0f / maskScale.y, 1.0f / maskScale.z);
 	}
 
@@ -54,7 +61,7 @@ public class MaskShrink : MonoBehaviour {
 
         Vector3 maskScale = defaultScale;
         maskRect.transform.localScale = maskScale;
-        triggerCollision.size = new Vector2(maskScale.x, maskScale.y);
+        triggerCollision.transform.localScale = triggerCollisionDefaultScale;
         maskImage.transform.localScale = new Vector3(1.0f / maskScale.x, 1.0f / maskScale.y, 1.0f / maskScale.z);
     }
 }
