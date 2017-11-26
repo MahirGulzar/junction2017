2f2ddff18b7ce3e6fa4e31b736cbe21a8143113f
Mahir Gulzar <mahirgulzar@gmail.com>
2017-11-26 04:03:48 +0200

    mhr

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    
+
+    public static SoundManager Instance;
+
+    public AudioSource OneShotLooper;
+
+    public AudioClip Skating;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+    public void PlayOneShotLoop(AudioClip audiofile)
+    {
+        OneShotLooper.clip = audiofile;
+        OneShotLooper.Play();
+    }
+
+    public void StopOneShotLoop()
+    {
+        OneShotLooper.Stop();
+    }
+
+
+}
