ddfe77974e89273abf7847b8cc891e4eaed1e452
Netzia <Netzia@users.noreply.github.com>
2017-11-26 02:43:58 +0200

    Alpha of the ice field if now also lerped

--- Assets/Scripts/MaskShrink.cs
+++ Assets/Scripts/MaskShrink.cs
@@ -9,12 +9,17 @@ public class MaskShrink : MonoBehaviour {
     public PolygonCollider2D triggerCollision;
     public Image maskImage;
     public RectTransform shadowImageRect;
+    public Image shadowImage;
     public GameManager gameManager;
     public float shrinkingPeriod;
     public float minIceFieldScale;
+    public float minAlpha;
+    public float startingAlpha;
     private float currentTime;
     Vector3 defaultScale;
     Vector3 triggerCollisionDefaultScale;
+    Color maskImageDefaultColor;
+    Color shadowImageDefaultColor;
     
 
 
@@ -34,6 +39,8 @@ public class MaskShrink : MonoBehaviour {
         currentTime = 0.0f;
         defaultScale = maskRect.transform.localScale;
         triggerCollisionDefaultScale = triggerCollision.transform.localScale;
+        maskImageDefaultColor = maskImage.color;
+        shadowImageDefaultColor = shadowImage.color;
     }
 	
 	// Update is called once per frame
@@ -41,6 +48,7 @@ public class MaskShrink : MonoBehaviour {
     {
         currentTime += Time.deltaTime;
         float shrinkFactor = Mathf.Min(currentTime / shrinkingPeriod, 1.0f);
+        float alpha = (1.0f - shrinkFactor) * startingAlpha;
         Vector3 maskScale = (1.0f - shrinkFactor) * defaultScale;
         Vector3 collisionScale = (1.0f - shrinkFactor) * triggerCollisionDefaultScale;
 
@@ -50,12 +58,19 @@ public class MaskShrink : MonoBehaviour {
         collisionScale.x = Mathf.Max(collisionScale.x, 0.0001f);
         collisionScale.y = Mathf.Max(collisionScale.y, 0.0001f);
         collisionScale.z = Mathf.Max(collisionScale.z, 0.0001f);
+        alpha = Mathf.Max(alpha, minAlpha);
 
         maskRect.transform.localScale = maskScale;
         shadowImageRect.transform.localScale = maskScale;
         triggerCollision.transform.localScale = collisionScale;
         maskImage.transform.localScale = new Vector3(1.0f / maskScale.x, 1.0f / maskScale.y, 1.0f / maskScale.z);
+        Color oldCol = maskImage.color;
+        oldCol.a = alpha;
+        maskImage.color = oldCol;
 
+        oldCol = shadowImage.color;
+        oldCol.a = alpha;
+        shadowImage.color = oldCol;
     }
 
     void resetScales(PlayerType playerType)
@@ -67,5 +82,7 @@ public class MaskShrink : MonoBehaviour {
         shadowImageRect.transform.localScale = maskScale;
         triggerCollision.transform.localScale = triggerCollisionDefaultScale;
         maskImage.transform.localScale = new Vector3(1.0f / maskScale.x, 1.0f / maskScale.y, 1.0f / maskScale.z);
+        maskImage.color = maskImageDefaultColor;
+        shadowImage.color = shadowImageDefaultColor;
     }
 }
